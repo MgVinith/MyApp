ae4540d79df4c23c20cbd46ebd70638be7eebb95
MgVinith <125434600+MgVinith@users.noreply.github.com>
2023-02-15 12:11:21 +0530

    my first api
    building api

---
+++ app.js
@@ -0,0 +1,27 @@
+//import express
+let express  = require("express");
+
+const mongodb = require("./mongodb/mongodb.utli");
+
+//initialize the app
+
+let app = express();
+let port = 8080;
+let apiRoutes = require('./routse/api-routes')
+
+//Middleware
+
+//This helps the express middleware to handle the json body request end point.
+app.use(express.json());
+
+app.use('/api',apiRoutes)
+
+app.get('/', (req, res)=>{
+    res.status(200).json("Hello this is my fisrt web server");
+})
+
+mongodb.connnect();
+
+app.listen(port, ()=>{
+    console.log("Running app on port ", port);
+})
